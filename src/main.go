1de323cd2a6f605cc386d3f29c13e4e49fdbf2c3
Bastien Gounon <45844674+bgounon@users.noreply.github.com>
2019-08-30 00:57:04 +0200

    retrieve all videos from a palylist ID

--- src/main.go
+++ src/main.go
@@ -1,6 +1,66 @@
 package main
 
-func main ()
-{
-	fmt.Println("test")
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+)
+
+// YTVideo : contains useful info about a given video
+type YTVideo struct {
+	ID    string
+	Title string
+}
+
+func main() {
+
+	b, err := ioutil.ReadFile(".APIKEY")
+	if err != nil {
+		fmt.Print(err)
+	}
+	apiKey := strings.TrimSuffix(string(b), "\n")
+
+	ctx := context.Background()
+	youtubeService, err := youtube.NewService(ctx, option.WithAPIKey(apiKey))
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	playListID := "PLxctXAfeP5AHQ_LMXVET5FJYs8OVQFEIN"
+
+	call := youtubeService.PlaylistItems.List("snippet")
+	call = call.PlaylistId(playListID)
+	call = call.MaxResults(50)
+	playList, err := call.Do()
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var videos []YTVideo
+
+	for ok := true; ok; ok = !(playList.NextPageToken == "") {
+		for _, item := range playList.Items {
+			var video YTVideo
+			video.ID = item.Id
+			video.Title = item.Snippet.Title
+			videos = append(videos, video)
+		}
+
+		playList, err = call.PageToken(playList.NextPageToken).Do()
+		if err != nil {
+			fmt.Print(err)
+		}
+	}
+
+	jsonVideos, err := json.Marshal(videos)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println(string(jsonVideos))
 }
